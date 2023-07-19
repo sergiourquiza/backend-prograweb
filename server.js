15ba2a1f5348d3c0cd27e861066e11936e514159
Steven Correa <97488936+Aegisystem@users.noreply.github.com>
2023-07-19 15:49:36 -0500

    Reserva de citas búsqueda working

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 const port = process.env.PORT || 3001;
@@ -92,6 +92,155 @@ app.post('/api/login', async (req, res) => {
     }
 });
 
+app.get("/agregar-cita", async function (req, res) {
+    const { profesor, alumno, curso, diaSemana, horaInicio, horaFin, enlace, estado } = req.query;
+  
+    try {
+      const nuevaCita = await Cita.create({
+        Profesor: profesor,
+        Alumno: alumno,
+        Curso: curso,
+        DiaSemana: diaSemana,
+        HoraInicio: horaInicio,
+        HoraFin: horaFin,
+        Enlace: enlace,
+        Estado: estado,
+      });
+  
+      res.send("Cita agregada satisfactoriamente");
+    } catch (error) {
+      console.log("Error al agregar cita:", error);
+      res.status(500).send("Error al agregar cita");
+    }
+});
+
+app.get("/mostrar-citas", async function (req, res) {
+  const citas = await Cita.findAll();
+  res.send(citas);
+});
+
+app.get("/eliminar-cita/:id", async function (req, res) {
+  const id = req.params.id;
+  await Cita.destroy({
+    where: {
+      CitasID: id,
+    },
+  });
+  res.send("Cita eliminada");
+});
+
+app.get("/guardar-horario/:dia/:horaInicio/:horaFin/:enlace", async function(req, res) {
+  let dia = req.params.dia;
+  let horaInicio = req.params.horaInicio;
+  let horaFin = req.params.horaFin;
+  let enlace = req.params.enlace;
+
+  await Horario.create({
+    dia: dia,
+    horaInicio: horaInicio,
+    horaFin: horaFin,
+    enlace: enlace
+  });
+
+  res.send("Horario de atención creado satisfactoriamente");
+});
+
+app.get("/listar-horarios", async function(req, res) {
+  const horarios = await Horario.findAll();
+
+  res.send(horarios);
+});
+
+app.get("/eliminar-horario/:horarioId", async function(req, res) {
+  await Horario.destroy({
+    where: {
+      horarioId: req.params.horarioId
+    }
+  });
+
+  res.send("Horario de atención eliminado");
+});
+
+app.get('/docentes-disponibles/init', async (req, res) => {
+  try {
+    // Crea al menos 5 docentes diferentes en la base de datos usando bulkCreate
+    await ProfesorDisp.bulkCreate([
+      {
+        name: 'Pepe',
+        university: 'Universidad de Lima',
+        course: 'Ingeniería de Sistemas',
+        dias_disponible: ['lunes', 'jueves', 'viernes', 'sabado']
+      },
+      {
+        name: 'Steven',
+        university: 'Universidad de Lima',
+        course: 'Programación Web',
+        dias_disponible: ['martes', 'jueves', 'sabado']
+      },
+      {
+        name: 'Sergio',
+        university: 'Universidad de Ibagué',
+        course: 'Programación Móvil',
+        dias_disponible: ['lunes', 'martes']
+      },
+      {
+        name: 'Valentina',
+        university: 'Universidad de Lima',
+        course: 'Programación Web',
+        dias_disponible: ['lunes', 'miercoles', 'viernes']
+      },
+      {
+        name: 'Random',
+        university: 'Universidad de Lima',
+        course: 'Random Course',
+        dias_disponible: ['martes', 'jueves', 'sabado']
+      }
+    ]);
+
+    res.send("Docentes creados satisfactoriamente");
+  } catch (e) {
+    res.status(500).send("Error al agregar docentes");
+  }
+});
+
+const normalizeString = (str) => diacritics.remove(str).toLowerCase();
+
+app.post('/docentes-disponibles', async (req, res) => {
+  const { search, fecha } = req.body;
+
+  try {
+    let filteredData;
+    if (search) {
+      // Realiza la búsqueda por nombre si se proporciona un valor de búsqueda
+      filteredData = await ProfesorDisp.findAll({
+        where: {
+          name: {
+            [Op.iLike]: `%${search}%`
+          }
+        }
+      });
+    } else if (fecha) {
+      // Realiza la búsqueda por fecha si se proporciona una fecha en el cuerpo de la solicitud
+      const normalizedFecha = normalizeString(fecha);
+      console.log("Esta es la fecha " + normalizedFecha)
+      filteredData = await ProfesorDisp.findAll({
+        where: {
+          dias_disponible: {
+            [Op.contains]: [normalizedFecha]
+          }
+        }
+      });
+      console.log("Encontré: " + filteredData.toString())
+    } else {
+      // Si no se proporciona ni el valor de búsqueda ni la fecha, se envía toda la lista de docentes disponibles
+      filteredData = await ProfesorDisp.findAll();
+    }
+
+    res.send(filteredData);
+  } catch (e) {
+    res.status(500).send("Error al obtener docentes");
+  }
+});
 
 app.listen(port, () => {
     console.log(`Activado en el puerto: ${port}`)
